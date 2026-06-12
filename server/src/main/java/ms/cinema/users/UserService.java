37687beeb89ab92b575692bec89dcee3ef6d72d4
Maciej-Stawiarz <precariousDuck@protonmail.com>
2026-06-12 15:07:47 +0200

    feature: Created User API. Added Swagger to it. (#7)

---
+++ server/src/main/java/ms/cinema/users/UserService.java
@@ -0,0 +1,64 @@
+package ms.cinema.users;
+
+import lombok.RequiredArgsConstructor;
+import ms.cinema.exceptions.NotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+	
+	private final UserRepository repository;
+	
+	public List<User> getAll() {
+		return repository.findAll();
+	}
+	
+	public User get(Long id) {
+		if (id == null) {
+			throw new IllegalArgumentException("User's id should be present to fetch it");
+		}
+		
+		return repository
+				.findById(id)
+				.orElseThrow(() -> new NotFoundException(String.format("Could not find a user with id: %d", id)));
+	}
+	
+	public User save(User user) {
+		if (user == null) {
+			throw new IllegalArgumentException("Object cannot be null to be saved");
+		}
+		if (user.getId() != null) {
+			throw new IllegalArgumentException("User's id should be null when saving new entity");
+		}
+		
+		return repository.save(user);
+	}
+	
+	public User update(User user) {
+		if (user == null) {
+			throw new IllegalArgumentException("Object cannot be null to be updated");
+		}
+		if (user.getId() == null) {
+			throw new IllegalArgumentException("User's id should not be null to update the entity");
+		}
+		if (!repository.existsById(user.getId())) {
+			throw new IllegalArgumentException("There is no user with given id to update");
+		}
+		
+		return repository.save(user);
+	}
+	
+	public void delete(Long id) {
+		if (id == null) {
+			throw new IllegalArgumentException("User's id should not be null to delete the entity");
+		}
+		if (!repository.existsById(id)) {
+			throw new IllegalArgumentException("There is no user with given id to delete");
+		}
+		
+		repository.deleteById(id);
+	}
+}
